c518d337f3137b6aa4852b9b1430d089093a27df
Miguel Sanchez Gomez <mgomez@kl1.head.cm.kestrel.hpc.nrel.gov>
2025-02-17 12:18:27 -0700

    Add IEA 15MW and IEA 11MW wind turbines to non-rotating actuator disk

--- WRF/external/io_grib1/MEL_grib1/FTP_getfile.c
+++ WRF/external/io_grib1/MEL_grib1/FTP_getfile.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "dprints.h"		/* Debug printing & function prototypes*/
 #include "gribfuncs.h"		/* function prototypes */
